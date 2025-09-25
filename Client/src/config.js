cf3719314d6653ababdfdf9587c7f05a10fddbf2
shashankverma16 <34279392+shashankverma16@users.noreply.github.com>
2025-09-26 00:44:15 +0530

    Update config.js

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,5 +1,5 @@
 // src/config.js
 export const API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://yhai-server.vercel.app' // deployed backend
-    : 'http://localhost:5000';          // local dev
+    ? 'https://yhai-goa-k6hk.vercel.app/' // deployed backend
+    : 'https://yhai-goa.vercel.app/';          // local dev
