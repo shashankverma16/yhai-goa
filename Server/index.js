dff403e7274bd23a15ae361ce7fa04439de07cca
shashankverma16 <34279392+shashankverma16@users.noreply.github.com>
2025-10-21 22:25:54 +0530

    Update index.js
    updated the / after origins

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ Enable CORS to allow requests from your React app
 app.use(cors({
-    origin: 'https://yhai-goa.vercel.app/', // change this to your React app's address
+    origin: 'https://yhai-goa.vercel.app', // change this to your React app's address
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
