2ed554921a9f1d070323d50a318f6de0eb4349f9
shashankverma16 <34279392+shashankverma16@users.noreply.github.com>
2025-10-21 22:11:44 +0530

    Update config.js
    updated the link for backend

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,5 +1,5 @@
 // src/config.js
 export const API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://yhai-goa-k6hk.vercel.app/' // deployed backend
+    ? 'https://yhai-goa-1.onrender.com/' // deployed backend
     : 'https://yhai-goa.vercel.app/';          // local dev
